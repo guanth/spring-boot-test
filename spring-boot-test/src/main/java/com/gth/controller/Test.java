14a384c81a5c11d98621302cf3511edda8197ec0
guanth <2859000700@qq.com>
2017-12-02 20:05:41 +0800

    测试git

---
+++ spring-boot-test/src/main/java/com/gth/controller/Test.java
@@ -0,0 +1,9 @@
+package com.gth.controller;
+
+import org.springframework.boot.SpringApplication;
+
+public class Test {
+	public static void main(String[] args) {
+		SpringApplication.run(TestController.class, args);
+	}
+}
